3f09150dc5146d31ee5ed5616d21ccc1878edc0f
Igor Tverdokhleb <itvrd2@yandex.ru>
2017-05-12 13:36:42 +0300

    prefix

--- routes/web.php
+++ routes/web.php
@@ -10,33 +10,30 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['prefix' => 'basic/v1'], function () {
+    Route::any('auth', 'AuthController@auth');
+    Route::any('logout', 'AuthController@logout');
 
-Route::any('auth', 'AuthController@auth');
-Route::any('logout', 'AuthController@logout');
+    Route::group(['middleware' => 'auth_json'], function () {
+        Route::any('categories', 'CategoryController@index');
+        Route::any('categories/add', 'CategoryController@store');
+        Route::any('categories/edit', 'CategoryController@edit');
+        Route::any('categories/del', 'CategoryController@destroy');
+        Route::any('transcat', 'CategoryController@transcat');
 
-Route::group(['middleware' => 'auth_json'], function() {
-    Route::any('categories', 'CategoryController@index');
-    Route::any('categories/add', 'CategoryController@store');
-    Route::any('categories/edit', 'CategoryController@edit');
-    Route::any('categories/del', 'CategoryController@destroy');
-    Route::any('transcat', 'CategoryController@transcat');
+        Route::any('categories/synch', 'CategoryController@synch');
+        Route::any('categories/{id}', 'CategoryController@show');
 
-//“data”: [ {“id”:<id>, “title”: <title>}, ...]
-    Route::any('categories/synch', 'CategoryController@synch');
-    Route::any('categories/{id}', 'CategoryController@show');
+        Route::any('balance', 'UsersController@balance');
 
-    Route::any('balance', 'UsersController@balance');
+        Route::any('transactions', 'OperationsController@index');
+        Route::any('transactions/add', 'OperationsController@store');
+        Route::any('transactions/edit', 'OperationsController@edit');
+        Route::any('transactions/del', 'OperationsController@destroy');
+        Route::any('transactions/synch', 'OperationsController@synch');
 
-    Route::any('transactions', 'OperationsController@index');
-    Route::any('transactions/add', 'OperationsController@store');
-    Route::any('transactions/edit', 'OperationsController@edit');
-    Route::any('transactions/del', 'OperationsController@destroy');
-    Route::any('transactions/synch', 'OperationsController@synch');
-
-
-    Route::any('items/add', 'ItemController@store');
-    Route::any('items/delete', 'ItemController@destroy');
-    Route::any('items', 'ItemController@itemsByType');
-
-
+        Route::any('items/add', 'ItemController@store');
+        Route::any('items/delete', 'ItemController@destroy');
+        Route::any('items', 'ItemController@itemsByType');
+    });
 });
